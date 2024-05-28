8fe32e49811a8df5e980822566b6b0913a58d5bb
Mircea Niculae <mircea.bogdan0517@gmail.com>
2024-05-28 10:51:58 +0300

    Add First Component

---
+++ src/components/MyFirstComponent.js
@@ -0,0 +1,11 @@
+function MyFirstComponent() {
+    return (
+        <div>
+            <h1>Hello, Mircea!!</h1>
+            <p>Start learning React!</p>
+        </div>
+    );
+}
+
+
+export default MyFirstComponent
