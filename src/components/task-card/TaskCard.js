8e81b43504a4a6bfb92552e909b44588291683f0
Mircea Niculae <mircea.bogdan0517@gmail.com>
2024-05-30 15:25:26 +0300

    Adding Props

--- src/components/task-card/TaskCard.js
+++ src/components/task-card/TaskCard.js
@@ -1,25 +1,26 @@
 import "./TaskCard.css";
 
-function TaskCard() {
-    return <div className = "card-wrapper">
+function TaskCard(props) {
+    return (
+         <div className = "card-wrapper">
         <div className = "card-header"> 
-            <p className="task-id">T-1</p>
+            <p className="task-id">{props.id}</p>
             <div className = "badge">
-                <p>Todo</p>
+                <p>{props.status}</p>
             </div>
         </div>
         <div className="card-content">
-            <p>Create a Design System for Enum Workspace.</p>
+            <p>{props.name}</p>
 
         </div>
         <div className="card-footer">
-            <div>
-            <p>Due Date</p>
-            <p>24/ 01/ 2021</p>
+            <div className="due-date">
+                <p>Due Date</p>
+                <p>{props.dueDate.toLocaleDateString()}</p>
             </div>
-
         </div>
     </div>
+    );
 }
 
 export default TaskCard;
